a17314e11768f82dfa8d4e71bcacb88bf93e2c41
Boadu Aaron Jeffrey Bosomepem <jeffreyaaron84@gmail.com>
2025-01-02 23:06:58 +0000

    refactor: move server logic from api/index.js to index.js and update routes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require('cors');
-const connectDB = require("../server/config/db");
-const {jobsRouter} = require("../server/routers/Job.router");
+const connectDB = require("./server/config/db");
+const {jobsRouter} = require("./server/routers/Job.router");
 
 const PORT =  process.env.PORT || 3000;
 
@@ -26,12 +26,12 @@ app.use((req, res, next) => {
 // Connect to MongoDB
 connectDB();
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send("<h1>Welcome to Job Portal API.</h1> <h2>If this is not your for you to use, you are not welcome.</h2>");
 });
 
 // Routes
-app.use("/api/jobs", jobsRouter);
+app.use("/jobs", jobsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
