8ef3288abb5dd6d80791d7375c0d06e948659ea0
Boadu Aaron Jeffrey Bosomepem <jeffreyaaron84@gmail.com>
2025-01-02 17:02:13 +0000

    fix: update CORS origin to match production frontend URL

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:4000', // Replace with your frontend's origin
+    origin: 'https://devjobs-rouge.vercel.app', // Replace with your frontend's origin
 }));
 app.use(express.json());
 
